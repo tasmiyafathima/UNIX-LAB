5befca557d50d31c512868e66ccdcd3d6d1f8d6e
tasmiyafathima <67852207+tasmiyafathima@users.noreply.github.com>
2021-12-01 19:45:47 +0530

    Add files via upload

---
+++ Program-4/program4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Enter the three numbers"
+read num1 num2 num3
+if [ $num1 -gt $num2 -a $num1 -gt $num3 ]
+then 
+echo "THe largest of three numbers is $num1"
+elif [ $num2 -gt $num1 -a $num2 -gt $num3 ]
+then
+ echo "The largest of three numbers is $num2"
+ else
+ echo "The largest of three numbers is $num3"
+ fi
+ 
