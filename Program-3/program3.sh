c6fe5c3dcb015f92470c9b9cb1834fd54d04ab7a
tasmiyafathima <67852207+tasmiyafathima@users.noreply.github.com>
2021-12-01 19:45:25 +0530

    Add files via upload

---
+++ Program-3/program3.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Enter the number"
+read num
+if [ $num -gt 0 ];then
+   echo "the number is positive"
+elif [ $num -lt 0 ];then
+   echo "the number is negative"
+else 
+  echo "The entered number is zero"
+fi        
